8e925ef862cdced45665f935b2c834c21c1e432b
Caleb Osei <oc.asante@gmail.com>
2022-09-08 18:28:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include <stdio.h> 
-
-/** 
-     * main - Prints 
+#include <stdio.h>
+/**
+     * main - prints
  *
-     * Return: Always 0 (Success) 
- */ 
-int main(void) 
-{ 
-     puts("\"Programming is like building a multilingual puzzle"); 
-       return (0); 
+     * Return: Always 0 (Success)
+ */
+int main(void)
+      puts("\"Programming is like building a multilingual puzzle"); 
+{
+     return (0);
 }
-
-
